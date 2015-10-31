3afc858a65f02747d4f1400d9dc9ce2c1e70fe9b
amit klein <amitkle001@gmail.com>
2015-10-31 16:45:24 +0200

    first commit

---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie : MonoBehaviour {
+    public float speed = 0.5f;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position = transform.position + new Vector3(0, 0, 0.5f) * Time.deltaTime;
+        transform.Translate(new Vector3(0, 0, speed) * Time.deltaTime);
+    }
+}
